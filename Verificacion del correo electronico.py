812c7713a75d579003bef18b03aac1c4783dd317
JoshCobra <hulls.reach-0p@icloud.com>
2024-10-03 23:28:35 -0600

    New Files  Organization = better

--- Verificacion del correo electronico.py
+++
@@ -1,9 +0,0 @@
-#Verificacion del correo electronico 
-
-correo = input("Ingresa un correo electronico: ")
-
-if correo.count('@') == 1:
-    print("El correo electronico es valido")
-else:
-    print("El correo electronico no es valido")
-    
