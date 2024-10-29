ba6449838af60210b0240cb30055e1d2282c1f15
JoshCobra <hulls.reach-0p@icloud.com>
2024-10-28 21:29:39 -0600

    Refactor Euclides class to calculate the MCD using the Euclidean method

--- Poo-Python/Ejercicios-Resueltos/Metodo-Euclides.py
+++ Poo-Python/Ejercicios-Resueltos/Metodo-Euclides.py
@@ -10,25 +10,27 @@ El programa debe asegurarse de dividir el numero mayor entre el menor.
 Con un menu donde se pueda elegir la opción de calcular el MCD o salir del programa.
 """
 
-class Euclides:
+
+class   Euclides:
     def __init__(self):
         #Definición de los atributos y valores enteros, están en 0 por que el usuario los va a ingresar
         self.num1 = 0
         self.num2 = 0
         #MCD es el resultado de la operación
         self.mcd = 0
-    
+
     def calcular_mcd(self):
         self.num1 = int(input("Ingrese el primer número: "))
         self.num2 = int(input("Ingrese el segundo número: "))
         #Se verifica cual es el número mayor para dividirlo entre el menor
         if self.num1 > self.num2:
-            self.mcd = self.num1/self.num2
+            self.mcd = self.num1 % self.num2
         else:
-            self.mcd = self.num2/self.num1
+            self.mcd = self.num2 % self.num1
 
         #Se verifica si el resultado de la división es un número entero
-        if self.mcd == int(self.mcd):
+        """
+         if self.mcd == int(self.mcd):
             print(f"El MCD de {self.num1} y {self.num2} es: {self.num2}")
         else:
             while self.mcd != int(self.mcd):
@@ -37,6 +39,15 @@ class Euclides:
                 self.num1 = self.num2
                 self.num2 = self.mcd
             print(f"El MCD de {self.num1} y {self.num2} es: {self.mcd}")
+"""
+        if self.mcd == 0:
+            print(f"El MCD de {self.num1} y {self.num2} es: {self.num2}")
+        else:
+            while self.mcd != 0:
+                self.mcd = self.num1 % self.num2
+                self.num1 = self.num2
+                self.num2 = self.mcd
+            print(f"El MCD de {self.num1} y {self.num2} es: {self.num1}")
 
     def menu(self):
         opcion = 0
