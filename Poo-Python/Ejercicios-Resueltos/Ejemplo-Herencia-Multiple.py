71a2fc0c313bd98d4b398eb4114efe2d32c061fd
JoshCobra <hulls.reach-0p@icloud.com>
2024-11-06 21:20:26 -0600

    Agregar ejemplos de herencia doble y multi-nivel en Python con clases Vehiculo, Coche y Deportivo

--- Poo-Python/Ejercicios-Resueltos/Ejemplo-Herencia-Multiple.py
+++ Poo-Python/Ejercicios-Resueltos/Ejemplo-Herencia-Multiple.py
@@ -1,3 +1,4 @@
+#Herencia Doble
 class Clase1:
     pass
 
@@ -10,6 +11,7 @@ class Clase3(Clase1, Clase2):
 # En este ejemplo la "Clase3" esta heredando a las clases "Clase1" y "Clase2", Herencia Doble
 
 
+#Herencia Multi-nivel
 class Clase1:
     pass
 
@@ -20,3 +22,21 @@ class Clase3(Clase2):
     pass
 
 #En este ejemplo la "Clase2" hereda a la "Clase1" y la "Clase3" hereda a la "Clase2", Herencia Multi-nivel
+
+#Ejemplo Herencia Multi-nivel
+class Vehiculo:
+    def conducir(self):
+        print('Este vehículo esta en movimiento')
+
+class Coche(Vehiculo):
+    def bocinazo(self):
+        print('El coche toca la bocina')
+
+class Deportivo(Coche):
+    def conducir_rapido(self):
+        print('El coche deportivo es muy rápido')
+
+mi_coche_deportivo = Deportivo()
+mi_coche_deportivo.bocinazo()
+mi_coche_deportivo.conducir()
+mi_coche_deportivo.conducir_rapido()
