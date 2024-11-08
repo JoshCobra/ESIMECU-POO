0966fcdad3951cba37fd4f96b55c4fb3432b3b7c
JoshCobra <hulls.reach-0p@icloud.com>
2024-11-07 23:23:06 -0600

    Agregar clases Cuenta, PlazoFijo y CajaAhorro con métodos para gestionar atributos y calcular intereses

---
+++ Poo-Python/Ejercicios-Resueltos/Clase-Cuenta-PlazoFijo-CajadeAhorro.py
@@ -0,0 +1,6 @@
+# Desarrollar un programa que conste de una clase padre Cuenta y dos subclases PlazoFijo y CajaAhorro. 
+# Definir los atributos titular y cantidad y un método para imprimir los datos en la clase Cuenta. 
+# La clase CajaAhorro tendrá un método para heredar los datos y uno para mostrar la información. 
+# La clase PlazoFijo tendrá dos atributos propios, plazo e interés. 
+# Tendrá un método para obtener el importe del interés (cantidad*interés/100) y otro método para mostrar la información, datos del titular plazo, interés y total de interés. 
+# Crear al menos un objeto de cada subclase. 
