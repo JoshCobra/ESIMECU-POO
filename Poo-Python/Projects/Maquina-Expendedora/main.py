d568c80b96d52344d0033ff46819b56bf9588768
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-05 13:02:55 -0600

    Modificar clase Producto para establecer valor predeterminado en refrigerado y actualizar instancias; agregar clase Bandeja con métodos para gestionar productos.

--- Poo-Python/Projects/Maquina-Expendedora/main.py
+++ Poo-Python/Projects/Maquina-Expendedora/main.py
@@ -1,5 +1,5 @@
 class Producto:
-    def __init__(self, nombre, precio, stock, refrigerado):
+    def __init__(self, nombre, precio, stock, refrigerado=False):
         self.nombre = nombre
         self.precio = precio
         self.stock = stock
@@ -15,23 +15,49 @@ class Producto:
             print(f"{self.nombre} - ${self.precio} (Disponibles: {self.stock}, No Refrigerado)")
     
 
-p1 = Producto("sabritas",22.50, 4, False)
-p2 = Producto("coca cola", 23, 5, True)
-p3 = Producto("doritos", 23.14, 0, False)
-p1.revisar_disponibilidad() #sabritas - $22.5 (Disponibles: 4, No Refrigerado)
-p2.revisar_disponibilidad() #coca cola - $23 (Disponibles: 5, Refrigerado)
-p3.revisar_disponibilidad() #doritos - $23.14 (Disponibles: 0, No Refrigerado)
+sabritas = Producto("sabritas",22.50, 4)
+cocacola = Producto("coca cola", 23, 5, True)
+doritos = Producto("doritos", 23.14, 0)
+
+sabritas.revisar_disponibilidad() #sabritas - $22.5 (Disponibles: 4, No Refrigerado)
+cocacola.revisar_disponibilidad() #coca cola - $23 (Disponibles: 5, Refrigerado)
+doritos.revisar_disponibilidad() #doritos - $23.14 (Disponibles: 0, No Refrigerado)
+
 
 class Bandeja:
-    def __init__(self, p1, capacidad):
-        self.p1 = p1
+    def __init__(self, capacidad, refrigerada=False):
         self.capacidad = capacidad
-
-    def agregar_producto(p1):
-        pass
+        self.refrigerada = refrigerada
+        self.productos = [] #Array de productos en la bandeja
 
     def mostrar_productos(self):
+        print(f"Productos que hay en la bandeja {self.productos}")
+
+    def agregar_producto(self, producto):
+        return self.productos.append(producto)
+    
+    def retirar_producto(self, nombre_producto):
+        # Para retirar un producto
         pass
 
-    def seleccionar_productos(self):
+bandeja_snacks = Bandeja(10)
+bandeja_snacks.agregar_producto(sabritas)
+bandeja_snacks.mostrar_productos()
+
+
+class MaquinaExpendedora:
+    def __init__(self, bandejas, cambio_disponible):
+        self.bandejas = bandejas
+        self.cambio_disponible = cambio_disponible
+    
+    def mostrar_productos(self):
+        # Lógica para listar productos disponibles
+        pass
+    
+    def insertar_dinero(self, cantidad):
+        # Lógica para aceptar dinero
+        pass
+    
+    def seleccionar_producto(self, codigo_bandeja, nombre_producto):
+        # Lógica para seleccionar y dispensar un producto
         pass
