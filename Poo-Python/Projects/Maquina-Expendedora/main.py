73f68478134fc244b21b97bb4f179d3e7afa43d7
JoshCobra <hulls.reach-0p@icloud.com>
2024-12-07 13:41:55 -0600

    Agregar clase Bandeja con métodos para agregar, obtener y mostrar productos

--- Poo-Python/Projects/Maquina-Expendedora/main.py
+++ Poo-Python/Projects/Maquina-Expendedora/main.py
@@ -1,3 +1,4 @@
+# Clase Producto
 class Producto:
     def __init__(self, nombre, precio, stock, bandeja, categoria, refrigerado=False):
         self.nombre = nombre
@@ -15,10 +16,21 @@ class Producto:
 # Doritos = Producto("Doritos",21.90,7,2,"snack")
 # print(Doritos)
 
+# Clase Bandeja
 class Bandeja:
     def __init__(self, codigo):
         self.codigo = codigo
         self.productos = {} #Diccionario para los productos
 
-    def agregar_producto():
-        pass
+    def agregar_producto(self, posicion, producto):
+        if posicion in self.productos:
+            print(f"Ya se encuentra un producto en la posicion {posicion}")
+        else :
+            self.productos[posicion] = producto
+
+    def obtener_producto(self,posicion):
+        self.productos.get(posicion,None) # método get para obtener el producto indicando en su posición
+
+    def mostrar_productos(self):
+        for posicion, producto in self.productos.items():
+            print(f"{self.codigo}{posicion}: {producto}")
