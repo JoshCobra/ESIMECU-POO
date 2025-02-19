44f0a5ae8500ad891efe7a7e18c28500d7bc0a85
JoshCobra <hulls.reach-0p@icloud.com>
2025-02-19 07:18:18 -0600

    Mejora la documentación y corrige el nombre del método 'tamano' a 'tamanio' en la clase Pila

--- Compiladores/Unidad-1/pilas.py
+++ Compiladores/Unidad-1/pilas.py
@@ -3,18 +3,23 @@
 class Pila:
     def __init__(self):
         self.items = []
+        # Se define un arreglo vacío de "items"
 
     def esta_vacia(self):
         return self.items == []
+        # En caso de que la pila este vacía retorna un verdadero o falso si tiene algo
 
     def apilar(self, item):
         self.items.append(item)
+        # Utilizamos append para agregar un nuevo item introducido por el usuario a la pila
 
     def desapilar(self):
         if not self.esta_vacia():
             return self.items.pop()
         else:
             return None
+        # Realiza una verificación si la pila esta vacía o no, en caso de no estarlo
+        # utiliza la propiedad pop para eliminar el ultimo elemento agregado a la pila
 
     def ver_tope(self):
         if not self.esta_vacia():
@@ -22,8 +27,9 @@ class Pila:
         else:
             return None
 
-    def tamano(self):
+    def tamanio(self):
         return len(self.items)
+        # Utiliza len para verificar la longitud de la pila, retorna el numero de longitud
 
 # Ejemplo de uso
 pila = Pila()
@@ -31,6 +37,6 @@ pila.apilar(1)
 pila.apilar(2)
 pila.apilar(3)
 print("Tope de la pila:", pila.ver_tope())
-print("Tamaño de la pila:", pila.tamano())
+print("Tamaño de la pila:", pila.tamanio())
 print("Desapilando:", pila.desapilar())
-print("Tamaño de la pila después de desapilar:", pila.tamano())
+print("Tamaño de la pila después de desapilar:", pila.tamanio())
