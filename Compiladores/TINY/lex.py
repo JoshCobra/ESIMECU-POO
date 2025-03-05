8b29af04fd1bccd6a8d3c68153ee064b13011fba
JoshCobra <hulls.reach-0p@icloud.com>
2025-03-05 07:54:44 -0600

    Agrega la clase Lexer con métodos para procesar caracteres, manejar espacios en blanco y comentarios, y obtener tokens.

---
+++ Compiladores/TINY/lex.py
@@ -0,0 +1,36 @@
+class Lexer:
+    def __init__(self, source):
+        self.source = source + '\n'
+        self.curChar = '' # Caracter actual en el string
+        self.curPos = -1 # Posición actual del caracter en el string
+        self.nextChar()
+
+    # Procesar el siguiente caracter
+    def nextChar(self):
+        self.curPos += 1
+        if self.curPos >= len(self.source):
+            self.curChar = '\0'
+        else:
+            self.curChar = self.source[self.curPos]
+
+    # Devolver el caracter 
+    def peek(self):
+        pass
+
+    # Token invalido, imprimir mensaje de error y salir
+    def abort(self, message):
+        pass
+
+    # Saltar espacios en blanco excepto salto de linea que
+    # indicara el final de una instrucción
+    def skipWhitespace(self):
+        pass 
+
+    # Saltar comentarios en el código
+    def skipComment(self):
+        pass
+
+    # Regresar el siguiente token
+    def getToken(self):
+        pass
+
