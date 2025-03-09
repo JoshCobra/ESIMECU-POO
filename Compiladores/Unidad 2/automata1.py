7d35a84d01682380b66975fd747df5dd7be7febe
JoshCobra <hulls.reach-0p@icloud.com>
2025-03-09 12:21:26 -0600

    Agrega método probar_cadena a la clase Automata para validar cadenas de prueba y mejorar la interacción con el usuario.

--- Compiladores/Unidad 2/automata1.py
+++ Compiladores/Unidad 2/automata1.py
@@ -13,24 +13,33 @@ class Automata:
         self.estadoInicial = 'q0'
         self.estadoAceptacion = ['q2']
 
+
     def procesar_cadena(self, cadena):
         estado_actual = self.estadoInicial            
         for simbolo in cadena:
-            if (estadoInicial, simbolo) in self.funcionTransicion:
-                estadoInicial = self.funcionTransicion[(estadoInicial, simbolo)]
+            if (estado_actual, simbolo) in self.funcionTransicion:
+                estado_actual = self.funcionTransicion[(estado_actual, simbolo)]
             else:
                 return False
         return estado_actual in self.estadoAceptacion
+    
+
+    def probar_cadena(self, test):
+        if automata.procesar_cadena(test):
+            print(f"La cadena '{test}' a sido aceptada por el automata.")
+        else:
+            print(f"La cadena '{test}' NO fue aceptada por el automata.")
+
         
 automata = Automata()
 prueba1 = 'aabb'
 prueba2 = 'abba'
 prueba3 = 'baba'
 
-def probar_cadena(cadena_prueba):
-    if automata.procesar_cadena(cadena_prueba):
-        print(f"La cadena '{cadena_prueba}' a sido aceptada por el automata.")
-    else:
-        print(f"La cadena '{cadena_prueba}' NO fue aceptada por el automata.")
+prueba4 = 'bbbb'
 
 
+automata.probar_cadena(prueba1)
+automata.probar_cadena(prueba2)
+automata.probar_cadena(prueba3)
+automata.probar_cadena(prueba4)
